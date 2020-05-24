8b7fafd45b6966b709bf14a54fe99de3606d9671
isnotafunction <ivana.cosgrove@gmail.com>
2020-05-24 19:34:20 +0100

    Add some basic templates

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -13,14 +14,23 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact page</h1>")
 }
 
+//temporary global var
+var homeTemplate *template.Template
+var contactTemplate *template.Template
+
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Home page</h1>")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		//not a good idea to panic here, will clean up later
+		panic(err)
+	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Contact page</h1>")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func notFound(w http.ResponseWriter, r *http.Request) {
@@ -30,6 +40,14 @@ func notFound(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	//parse the template when main starts
+	var err error
+	//here we don't use := so that we actually use the global scope vars
+	homeTemplate, err = template.ParseFiles("views/home.gohtml", "views/layouts/footer.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml", "views/layouts/footer.gohtml")
 	r := mux.NewRouter()
 	r.NotFoundHandler = http.HandlerFunc(notFound)
 	r.HandleFunc("/", home)
