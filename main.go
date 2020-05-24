ac1598862f2cf8532d53bf165efd88fa0b737b6c
isnotafunction <ivana.cosgrove@gmail.com>
2020-05-24 08:26:38 +0100

    Add a basic server with routing using gorilla/mux router

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	// fmt.Fprint(w, "<h1>Welcome to my awesome site</h1>")
+	fmt.Fprint(w, "<h1>Contact page</h1>")
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Home page</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Contact page</h1>")
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
+}
